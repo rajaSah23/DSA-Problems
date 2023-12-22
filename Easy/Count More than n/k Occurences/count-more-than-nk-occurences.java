a554b0bace2e67248f9ed63ee67630946c585045
rajaSah23 <120490224+rajaSah23@users.noreply.github.com>
2023-12-23 02:31:03 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count More than n/k Occurences/count-more-than-nk-occurences.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+
+            System.out.println(new Solution().countOccurence(arr, n, k));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution 
+{
+    //Function to find all elements in array that appear more than n/k times.
+    public int countOccurence(int[] arr, int n, int k) 
+    {
+        HashMap<Integer, Integer> hm= new HashMap<>();
+        int count=0;
+        for(int a:arr)
+            hm.put(a, hm.getOrDefault(a,0)+1);
+        
+        for(int a: hm.keySet()){
+            if(hm.get(a)>(n/k))
+                count++;
+        }
+        return count;
+    }
+}
