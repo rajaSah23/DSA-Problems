942ad5a8b1a76d5024191c29cb9f53c98981236b
rajaSah23 <120490224+rajaSah23@users.noreply.github.com>
2023-12-26 00:34:43 +0530

    Time: 448 ms (63.69%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select max(salary) as SecondHighestSalary 
-from Employee
-where salary < (select max(salary)
-                from Employee);
+# Using Window Function
+select max(salary) AS 'SecondHighestSalary'
+from  (select * , DENSE_RANK() OVER(order by salary desc) as sal_rank
+        from Employee) t
+where  t.sal_rank=2;
