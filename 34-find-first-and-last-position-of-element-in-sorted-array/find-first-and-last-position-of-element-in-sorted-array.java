7369d4ac66c21372b21f7cfbe65b73bf17caa62a
Raja Sah <120490224+rajaSah23@users.noreply.github.com>
2024-05-15 00:11:44 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (25.33%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,30 +1,16 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        //1. BruteForce O(n*n)
-        //2. Sliding Window O(n)
-        //3. Binary search O(logn):
-        int l=0,r=nums.length-1;
-        int[] ans= new int[2];
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(nums[mid]==target){
-                l=mid;
-                r=mid;
-                while(l>=0 && nums[l]==target)
-                    l--;
-                ans[0]=++l;
-                while(r<=nums.length-1&& nums[r]==target)
-                    r++;
-                ans[1]=--r;
+        int[] ans= {-1,-1};
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                ans[0]=i;
+                while(i<nums.length && nums[i]==target)
+                    i++;
+                ans[1]=i-1;
 
                 return ans;
-            }else if(target<nums[mid])
-                r=mid-1;
-            else
-                l=mid+1;
+            }
         }
-        ans[0]=-1;
-        ans[1]=-1;
         return ans;
     }
 }
