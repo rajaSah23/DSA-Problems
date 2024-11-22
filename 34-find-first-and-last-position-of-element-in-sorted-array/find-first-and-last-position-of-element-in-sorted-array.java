a1c9ac088bf7c4adf5249d6ca68a6e8717f42e8e
Raja Sah <120490224+rajaSah23@users.noreply.github.com>
2024-11-22 22:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (83.58%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans= {-1,-1};
-        int l=0,r=nums.length-1;
-        while(l<=r){
-            int mid= (l+r)/2;
-            if(nums[mid]==target){
-                int i=mid,j=mid;
-                while(i>=0 && nums[i]==target) i--;
-                while(j<nums.length && nums[j] == target) j++;
-                i++;
-                j--;
-                ans[0]=i;
-                ans[1]=j;
-                return ans;
+        int l=0, r=nums.length-1;
+        while(l<r){
+            int mid =(l+r)/2;
+            if(target <= nums[mid]){
+                r=mid;
+            }else{
+                l=mid+1;
             }
-            if(target<nums[mid]) r=mid-1;
-            else l= mid+1;
         }
-        return ans;
+        r=l;
+        while(r<nums.length && nums[r]==target) r++;
+        r--;
+        if(nums.length==0) return new int[]{-1,-1};
+        if(nums[l]!=target) return new int[]{-1,-1};
+        return  new int[]{l,r};
     }
 }
