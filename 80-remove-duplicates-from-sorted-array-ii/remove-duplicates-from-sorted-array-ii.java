199d7d3bf3bee98b991266ce63f5b2d66ef69dce
Raja Sah <120490224+rajaSah23@users.noreply.github.com>
2024-11-25 21:57:23 +0530

    Time: 1 ms (20.46%) | Memory: 44.4 MB (51.16%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -3,15 +3,11 @@ class Solution {
         int count=1;
         int last=0;
         for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[last] && count<2){
-                last++;
-                nums[last]=nums[i];
+            if( count < 2 || nums[last] != nums[i]){
+                nums[last+1] = nums[i];
+                if(nums[last] != nums[i]) count=0;
                 count++;
-            }
-            if(nums[i]>nums[last]){
                 last++;
-                nums[last]=nums[i];
-                count=1;
             }
         }
         return last+1;
