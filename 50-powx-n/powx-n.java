c8e03ca8e39dbcbbce9f0f0cb5870fa70f573330
Raja Sah <120490224+rajaSah23@users.noreply.github.com>
2024-12-09 23:18:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (5.54%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0) return 1;
+
+        //edge case:for n= +-2147483648
+        if(n == Integer.MIN_VALUE){
+        x = x * x;
+        n = n/2;
+        }
+
+        if(n<0){
+            x= 1/x;
+            n=-n;
+        }
+
+        if(n%2==0) return myPow(x*x,n/2);
+        else return x*myPow(x*x,n/2);
+    }
+}
