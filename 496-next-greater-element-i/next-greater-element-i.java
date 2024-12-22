b45bac7fd8e0cb53c6504aeef2aba7953ff983f2
Raja Sah <120490224+rajaSah23@users.noreply.github.com>
2024-12-22 11:21:53 +0530

    Time: 3 ms (91.88%) | Memory: 44.5 MB (12.58%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -2,15 +2,17 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         //Create a monotonic stack
         Stack<Integer> stk = new Stack<>();
-        int[] nextGreaterArr = new int[nums2.length];
+        // HashMap<element,nextGreater>
+        HashMap<Integer, Integer> hm = new HashMap<>();
         int i=nums2.length-1;
         while(i>=0){
             if(stk.isEmpty()){
-                nextGreaterArr[i] = -1;
+                 //will store in HashMap<element,nextGreater>
+                 hm.put(nums2[i],-1);
                 stk.push(nums2[i]);
                 i--;
             }else if(stk.peek()>nums2[i]){
-                nextGreaterArr[i]= stk.peek();
+                hm.put(nums2[i],stk.peek());
                 stk.push(nums2[i]);
                 i--;
             }else{
@@ -21,11 +23,7 @@ class Solution {
         //Now for num:nums1 find next greater in nextGreaterArr by idx
         int[] ans= new int[nums1.length];
         for(i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    ans[i] = nextGreaterArr[j];
-                }
-            }
+            ans[i] = hm.get(nums1[i]);
         }
         return ans;
     }
