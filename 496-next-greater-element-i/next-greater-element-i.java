c7237d5cca0ff1ba8a75812e72b373b49f339580
Raja Sah <120490224+rajaSah23@users.noreply.github.com>
2024-12-22 11:13:22 +0530

    Time: 6 ms (11.16%) | Memory: 43.2 MB (86.48%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,27 +1,29 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> stk= new Stack<>();
-        int[] arr= new int[nums2.length];
-
-        for(int i=nums2.length-1;i>=0;i--){
-            while(!stk.isEmpty() && stk.peek()<=nums2[i])
+        //Create a monotonic stack
+        Stack<Integer> stk = new Stack<>();
+        int[] nextGreaterArr = new int[nums2.length];
+        int i=nums2.length-1;
+        while(i>=0){
+            if(stk.isEmpty()){
+                nextGreaterArr[i] = -1;
+                stk.push(nums2[i]);
+                i--;
+            }else if(stk.peek()>nums2[i]){
+                nextGreaterArr[i]= stk.peek();
+                stk.push(nums2[i]);
+                i--;
+            }else{
                 stk.pop();
-            
-            if(stk.isEmpty())
-                arr[i]=-1;
-            else
-                arr[i]=stk.peek();
-            
-            stk.push(nums2[i]);
+            }
         }
 
-        //for ans
+        //Now for num:nums1 find next greater in nextGreaterArr by idx
         int[] ans= new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
+        for(i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
-                if(nums2[j]==nums1[i]){
-                    ans[i]=arr[j];
-                    break;
+                if(nums1[i]==nums2[j]){
+                    ans[i] = nextGreaterArr[j];
                 }
             }
         }
