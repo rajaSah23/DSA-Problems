35fecbfe0199bd874ddea8bf49fede6a319d9edc
Raja Sah <120490224+rajaSah23@users.noreply.github.com>
2025-01-18 19:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (90.85%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,9 +1,22 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        for(int a:arr){
-            if(a<=k) k++;
-            else break;
+        //1. find the index of arr in between our k'th missing lies in
+        //using binary search
+        int l=0;
+        int r=arr.length-1;
+        while(l<=r){
+            int mid= l+(r-l)/2;
+            int missingCounts = arr[mid] - mid - 1;
+            if(missingCounts<k){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
         }
-        return k;
+
+        //now we have indexes between our k'th missing lies in, ie: l,r
+        //2. find out k'th missing rom this interval
+        return l+k;
+
     }
 }
